b18083c66f6083b60716686d5f7d2c79a11023fa
Jitendra Kumar Singh <64576350+Jitendra077@users.noreply.github.com>
2020-07-21 11:16:38 +0530

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import {BrowserRouter as Router , Link , Route} from "react-router-dom";
-import LoginForm from "./LoginForm";
-
-const Welcome = () => {
-    return(
-        <div>
-            <h1>Welcome !!!</h1>
-            <Router>     
-           <Link to = "/Login">Logout </Link>
-           <Route path ="/Login" component = {LoginForm} />
-           </Router>
-        </div>
-    )
-}
-export default Welcome
+//error
